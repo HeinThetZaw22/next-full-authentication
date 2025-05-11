ec54ad13ec8924bea730dccf2b23584fc147e266
HeinThetZaw22 <heinthetzaw2212@gmail.com>
2025-05-11 19:56:45 +0630

    profile image added

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
 import LoginButton from "@/components/auth/login-button";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className=" flex h-full flex-col items-center justify-center">
-      <div className=" space-y-6 text-center">
-        <h1>Auth</h1>
-        <p>next authenication service</p>
+      <div className=" space-y-6 flex max-w-lg flex-col border border-gray-100 items-center justify-center rounded-md px-5 py-10 shadow-md text-center">
+        <Image src={"/profile.png"} width={120} className=" rounded-full object-cover" height={120} alt="profile" />
+        <h1>My Next Authenication Service</h1>
+        <p className=" text-sm text-gray-500">
+          Login and test two-factor auth, reset password and role-based access in setting
+        </p>
         <div>
           <LoginButton mode="modal" asChild>
-            <Button variant="secondary" size="lg">
-              Sign in
-            </Button>
+            <Button size="lg">Sign in</Button>
           </LoginButton>
         </div>
       </div>
